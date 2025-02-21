f78fff163fa999249f95824e03e347f72c9cc0e1
TranDucNghia123 <tranducnghia10m2@gmail.com>
2025-02-21 10:17:17 +0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function login()
+{
+  let t= tendangnhap.value
+  let m= matkhau.value
+  if (t=="ducnghia" && m=="0108")
+  {
+    window.open("house.html")
+    document.getElementById(compiler).style.display="block"
+  }
+}
